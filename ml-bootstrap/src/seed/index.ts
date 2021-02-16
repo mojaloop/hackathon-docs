5bc9b40b74067fec4f063df26242b4cbebc01483
Lewis Daly <lewisdaly@me.com>
2021-02-16 17:10:05 +1030

    feat: work on roadmap

--- ml-bootstrap/src/seed/index.ts
+++ ml-bootstrap/src/seed/index.ts
@@ -9,11 +9,11 @@ import { SeedCollection } from './types'
 
 const collections: Array<SeedCollection> = [
 
-  // hubSteps(config),
-  // oracleSteps(config),
+  hubSteps(config),
+  oracleSteps(config),
 
   // // // Generate a set of participant steps for each participant
-  // ...config.participants.map(p => makeParticipantSteps(p)(config)),
+  ...config.participants.map(p => makeParticipantSteps(p)(config)),
 
   // // Generate a set of party steps for DFSP participants
   ...config.participants
